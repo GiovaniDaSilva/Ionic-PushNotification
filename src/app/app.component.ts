49b5435d31499fc21f7e1ccd0a8eb81fdc05367a
Giovani da Silva <giovani.silva@govbr.com.br>
2021-02-18 14:18:36 -0300

    arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private push : Push,
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,23 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.inicializaFirebase();
     });
   }
+
+
+  private inicializaFirebase(){
+    const options: PushOptions = {
+      android:{
+        senderID: 'XXXXXXXXXXX'
+      }
+    }
+
+    const pushObject: PushObject = this.push.init(options)
+
+    pushObject.on('registration').subscribe(res => console.log(`${res.registrationId}`))
+    pushObject.on('notification').subscribe(res => console.log(`já chegou o disco voador: ${res.message}`))
+  }
+
 }
